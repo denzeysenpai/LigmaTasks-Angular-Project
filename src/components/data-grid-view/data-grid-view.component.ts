9f24e657b67e453ba29df17b7c1b0e57d5c6776e
Mark Solante <marksolante221@gmail.com>
2024-06-14 01:19:31 +0800

    Added constraints in input, just small things. I need to add functionality to login tomorrow, even if I have to create a user in MongoDB manually just so that I can test its functionality.
    But yes, connect to MongoDB, set up node and express, then add registration after

--- src/components/data-grid-view/data-grid-view.component.ts
+++ src/components/data-grid-view/data-grid-view.component.ts
@@ -47,24 +47,35 @@ export class DataGridViewComponent implements OnInit {
 
   SubmitInput(s : string) {
     const titleInput = document.getElementById('task-title-input') as HTMLInputElement
-    const contentInput = document.getElementById('#task-content-input') as HTMLTextAreaElement
 
     let titleValue : string = titleInput.value
     // let contentValue : string = contentInput.value
 
-    console.log('Title value: ' + titleValue)
-    console.log('Content value: ' + s)
-
-    this.DataAdapter.AddNewTask(titleValue, s, '1')
-    this.ClearInput()
+    if(titleValue.trim() != '' && s.trim() != '') {
+      console.log('Title value: ' + titleValue)
+      console.log('Content value: ' + s)
+  
+      this.DataAdapter.AddNewTask(titleValue, s, '1')
+      alert('Task added successfully!')
+      this.ClearInput()
+    }
+    else if (titleValue.trim() == '' && s.trim() == ''){
+      alert('Invalid submission detected!')
+      console.log('Invalid submission: user error')
+    }
+    else if (s.trim() == '') {
+      alert('Task content cannot be null!')
+      console.log('Invalid submission: user error')
+    }
+    else {
+      console.log('Invalid submission: user error')
+      alert('Title cannot be empty!')
+    }
   }
 
-  ClearInput() {
+  ClearInput(){
     const titleInput = document.getElementById('task-title-input') as HTMLInputElement
-    // const contentInput = document.getElementById('task-content-input') as HTMLTextAreaElement
-
     titleInput.value = ''
-    // contentInput.value = ''
     this.Refresh(this.filter1, this.filter2)
   }
 
